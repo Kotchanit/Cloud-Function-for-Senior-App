04c95fb4fa4bef8923a57ede5d321f2fbffbeba0
Benyapa Emsawat <benyapae57@email.nu.ac.th>
2017-10-04 18:35:21 +0700

    call chatroomID

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,16 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const database = admin.database()
+
+exports.checkchat = functions.database.ref('/chatrooms/{ChatroomID}/members').onCreate(event => {
+    const chatroomID = event.params.ChatroomID;
+    const name = event.data.val();
+    console.log("*************");
+    console.log(chatroomID);
+    console.log("------------");
+    console.log(name);
+})
