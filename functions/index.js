3af8c55b655e8711e0b3f9d7b28d0162a1fd2478
Benyapa Emsawat <benyapae57@email.nu.ac.th>
2017-11-03 12:04:52 +0700

    send notification complete

--- functions/index.js
+++ functions/index.js
@@ -11,31 +11,39 @@ exports.messagesComing = functions.database.ref('/chatrooms/{chatroomID}/message
     const chatroomID = event.params.chatroomID;
     const messageID = event.params.messageID;
     const message = event.data.val();
+    // console.log("***** messages onCreate *****");
+    // console.log("Chatroom ID: "+chatroomID);
+    // console.log("Message ID: "+messageID);
+    // console.log(message);
 
-    console.log("***** data is coming *****");
-    console.log(chatroomID);
-    console.log(messageID);
-    console.log(message);
-
-    firebase.database.ref('users').orderByChild('chatrooms/'+chatroomID).isEqual(true).on("child_added", function(snapshot) {
-      const user = snapshot.val();
-      if (snapshot.key() == message.senderID) {
-        return;
-      }
-      const fcmToken = user.data.fcmtoken;
-        console.log("***** fcm is coming *****");
-        console.log(fcmToken);
-        sendMessageNotification(message, fcmToken)
+    database.ref('users').orderByChild('chatrooms/'+chatroomID).equalTo(true).once('value', function(snapshot) {
+      snapshot.forEach(function(userSnapshot) {
+        const userID = userSnapshot.key;
+        const userData = userSnapshot.val().data;
+        if (userID != message.senderID && userData.hasOwnProperty('fcmtoken')) {
+          let token = userData.fcmtoken;
+          sendMessageNotification(message, token);
+        }
+      });
     });
 });
 
 
 function sendMessageNotification(message, fcmToken){
     const payload = {
-        notification: {
-        title : message.senderName,
-        body : message.text,
+      notification: {
+        title: message.senderName,
+        body: message.text,
+        sound: "activated",
         badge: "0"
-    }};
+      }
+    };
+    console.log(payload);
     admin.messaging().sendToDevice(fcmToken, payload)
+      .then(function (response) {
+        console.log('Successfully sent message:', response);
+      })
+      .catch(function (error) {
+          console.log('Error sending message:', error);
+      });
   }
